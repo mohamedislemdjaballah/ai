0659bf383fa7254ba552e221cb08a566bf9a9b01
mohamedislemdjaballah <med.islam07@hotmail.com>
2023-02-21 20:55:50 +0100

    added some structure to the class CreatBoard

--- AI/CreatABoard.java
+++ AI/CreatABoard.java
@@ -5,33 +5,60 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Stack;
+import java.util.Queue;
+
 
 public class CreatABoard extends JFrame implements ActionListener{
+    /* The Hegiht number of row cells and Width number of col cels never Change 
+     * So i just declared as static 'consts'
+     */
     static int height,width;
+
+    /* Some Colors and Fonts to use */
+    Font font = new Font("DIALOG",Font.PLAIN,20);
+    Color red = new Color(225, 0, 0);
+
+    /* Space and gameboard are just panels to help put 
+    in compenentsin he frame */
+
     JPanel space = new JPanel(new BorderLayout());
     JPanel gameBoard ;
     JButton DrawBoard = new JButton("Confirme Cords");
-    /* JPanel is the matrix of our Game */
+    
+    /* JPanel cells holds the matrix cells of our Game */
     ArrayList<JPanel> cells = new ArrayList<JPanel>();
     JPanel cell;
+
     // x and y store the JSpinner Object values to needed use
     JSpinner x = new JSpinner();
     JSpinner y = new JSpinner();
+    
     /* colors array list  */
     ArrayList<Color> colors = new ArrayList<Color>();
     /*Random value to chose from the colors list */
     Random rand = new Random(4);
+    /* Matrix Array*/
+    Cords cordinate;
+    ArrayList<Cords> matrixCells = new ArrayList<Cords>();
     
-    // let's cret a board first we need a method for that
+    
+    /* This method Creat the initial State of the game 
+     * where we hold the x and y cordinates
+    */
     void Board() {
         setTitle("Robot");
         setSize(800,600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         x.setBounds(30, 5, 45, 10);
         y.setBounds(110, 5, 45, 10);
+        
+        /* Adding the components to the space panel */
         space.add(x,BorderLayout.NORTH);
         space.add(y,BorderLayout.LINE_END);
         space.add(DrawBoard);
+
+        /* colors list declaration and initialising */
         colors.add(Color.red);
         colors.add(Color.black);
         colors.add(Color.white);
@@ -39,44 +66,51 @@ public class CreatABoard extends JFrame implements ActionListener{
         colors.add(Color.yellow);
         colors.add(Color.green);
         
+        /* Adding space to the Frame */
         setContentPane(space);
         DrawBoard.addActionListener(this);
-        
-    setVisible(true);
-    }
-    
-    void confirmCords(int width,int height){
-        x.setVisible(false);
-        y.setVisible(false);
-        gameBoard = new JPanel(new GridLayout(width,height));
-        for (int i=0 ;i<3;i++)
-            for (int j=0; j<3;j++){
-                cell = new JPanel();
-                cells.add(cell);    
-            }
-    int x=0;
-    for (int i=0 ;i<3;i++)
-        for (int j=0;j<3;j++)
-            {
-                gameBoard.add(cells.get(x));
-                x++;
-            }
-    space.add(gameBoard,BorderLayout.CENTER);
-    setVisible(true);
-    }
 
-    public void actionPerformed(ActionEvent e){  
-        width = (int)x.getValue();
-        height = (int)y.getValue();
-        x.setVisible(false);
-        y.setVisible(false);
-        DrawBoard.setVisible(false);
+        /*Seting Frame visibility to true */
+        setVisible(true);
+    }
+    /*Whene we Click on any event on the Frane this method runs
+     *by default to initiate the following commands
+     */
+    public void actionPerformed(ActionEvent e)
+    {  
+        /* Width and height takes the value of the 
+         * JSpinner 
+         */
+        if((int)x.getValue() > 0 && (int)y.getValue() > 0)
+        {
+            width = (int)x.getValue();
+            height = (int)y.getValue();
+            x.setVisible(false);
+            y.setVisible(false);
+            DrawBoard.setVisible(false);
+        }else{
+            System.err.println("enter a valid and x and y arguments");
+            JDialog errour = new JDialog();
+            JLabel err = new JLabel("NO CORDINATES FOR X AND Y");
+           
+            err.setFont(font);
+            err.setForeground(red);
+            errour.add(err);
+            errour.setSize(300,300);
+            setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
+            errour.setVisible(true);
+            
+
+        }
+
         gameBoard = new JPanel(new GridLayout(width,height));
         for (int i=0 ;i<width;i++)
             for (int j=0; j<height;j++)
             {
                 cell = new JPanel();
-                cells.add(cell);    
+                cells.add(cell);
+                cordinate = new Cords(i, j);
+                matrixCells.add(cordinate);
             }
     int x=0;
     for (int i=0 ;i<width;i++)
@@ -90,11 +124,13 @@ public class CreatABoard extends JFrame implements ActionListener{
     space.add(gameBoard,BorderLayout.CENTER);
     solve();
     space.setVisible(true);
-}  
+    }
     void solve(){
         int width = (int)x.getValue();
         int height = (int)y.getValue();
         System.out.println("("+width+","+height+")");
+        for (Cords cord : matrixCells)
+        System.out.println("("+cord.getX()+","+cord.getY()+")");
     }
     public static void main(String argv[]){
         try {
