f91ed2497750bf227363d11090a4d8a2d6a44c5f
mohamedislemdjaballah <med.islam07@hotmail.com>
2023-02-22 04:21:29 +0100

    added photos

--- AI/CreatABoard.java
+++ AI/CreatABoard.java
@@ -21,19 +21,20 @@ public class CreatABoard extends JFrame implements ActionListener{
 
     /* Space ,cordPanel and gameboard are just panels to help put 
     in compenentsin he frame */
-
     JPanel space = new JPanel(new BorderLayout());
     JPanel gameBoard ;
     JPanel cordPanel = new JPanel(new FlowLayout());
     
     /* Some needed Labels to Represent some details */
     JLabel cordinatLabel = new JLabel("Give The Cordinates X(col) then Y(row)");
+    
     /*DrawBoard Button */
     JButton DrawBoard = new JButton("Confirme Cords");
+    JButton StartSolving = new JButton("Start Game");
     
-    /* JPanel cells holds the matrix cells of our Game */
-    ArrayList<JPanel> cells = new ArrayList<JPanel>();
-    JPanel cell;
+    /* ImagePanel cells holds the matrix cells of our Game */
+    ArrayList<JLabel> cells = new ArrayList<JLabel>();
+    JLabel cell;
 
     // x and y store the JSpinner Object values to needed use
     JSpinner x = new JSpinner();
@@ -41,13 +42,25 @@ public class CreatABoard extends JFrame implements ActionListener{
     
     /* colors array list  */
     ArrayList<Color> colors = new ArrayList<Color>();
+    
     /*Random value to chose from the colors list */
     Random rand = new Random(4);
-    /* Matrix Array*/
-    Cords cordinate;
+    
+    /* Matrix Array and some well needed variables*/
+    int ref;
+    Cords cordinate,state,parent,start,goal;
     ArrayList<Cords> matrixCells = new ArrayList<Cords>();
-    
-    
+    /* Lets Draw some images  */
+    ImageIcon fire = new ImageIcon("fire.png");
+    ImageIcon water = new ImageIcon("sea.png");
+    ImageIcon sand = new ImageIcon("sand.png");
+    ImageIcon wall = new ImageIcon("wall.png");
+    // Image grass = Toolkit.getDefaultToolkit().createImage("../grass.png");
+    //gamePanel.drawImage(background, 0, 0, null);
+    /* Array List to Store images  */
+    // ArrayList<Image> obstacles = new ArrayList<Image>();
+    ArrayList<ImageIcon> obstacles = new ArrayList<ImageIcon>();
+
     /* This method Creat the initial State of the game 
      * where we hold the x and y cordinates
     */
@@ -71,7 +84,7 @@ public class CreatABoard extends JFrame implements ActionListener{
         space.add(cordPanel,BorderLayout.NORTH);
         space.add(DrawBoard);
 
-        /* colors list declaration and initialising */
+        /* colors list initialising */
         colors.add(Color.red);
         colors.add(Color.black);
         colors.add(Color.white);
@@ -79,6 +92,13 @@ public class CreatABoard extends JFrame implements ActionListener{
         colors.add(Color.yellow);
         colors.add(Color.green);
         
+        /*image List initialising */
+        obstacles.add(fire);
+        obstacles.add(wall);
+        obstacles.add(sand);
+        obstacles.add(water);
+        
+
         /* Adding space to the Frame */
         setContentPane(space);
         DrawBoard.addActionListener(this);
@@ -91,64 +111,143 @@ public class CreatABoard extends JFrame implements ActionListener{
      */
     public void actionPerformed(ActionEvent e)
     {  
-        /* Width and height takes the value of the 
-         * JSpinner 
-         */
-        if((int)x.getValue() > 0 && (int)y.getValue() > 0)
+        if(e.getSource()==DrawBoard)
         {
-            width = (int)x.getValue();
-            height = (int)y.getValue();
-            x.setVisible(false);
-            y.setVisible(false);
-            DrawBoard.setVisible(false);
-            cordinatLabel.setVisible(false);
-        }else{
-            /* */
-            System.err.println("enter a valid and x and y arguments");
-            JDialog errour = new JDialog();
-            errour.setTitle("Errour");
-            JLabel err = new JLabel("NO CORDINATES FOR X AND Y");
-            
-            err.setFont(font);
-            err.setForeground(red);
-            errour.add(err);
-            errour.pack();
-            setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
-            errour.setVisible(true);
-            
+            /* Width and height takes the value of the 
+            * JSpinner 
+            */
+            if((int)x.getValue() > 0 && (int)y.getValue() > 0)
+            {
+                width = (int)x.getValue();
+                height = (int)y.getValue();
+                x.setVisible(false);
+                y.setVisible(false);
+                DrawBoard.setVisible(false);
+                cordinatLabel.setVisible(false);
+            }else{
+                /* */
+                System.err.println("enter a valid and x and y arguments");
+                JDialog errour = new JDialog();
+                errour.setTitle("Errour");
+                JLabel err = new JLabel("NO CORDINATES FOR X AND Y");
+                
+                err.setFont(font);
+                err.setForeground(red);
+                errour.add(err);
+                errour.pack();
+                setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
+                errour.setVisible(true);
+                
 
+                }
+
+                gameBoard = new JPanel(new GridLayout(width,height));
+                for (int i=0 ;i<width;i++)
+                    for (int j=0; j<height;j++)
+                    {
+                        cell = new JLabel();
+                        cell.setOpaque(true);
+                        cell.setVerticalAlignment(JLabel.CENTER);
+                        cell.setHorizontalAlignment(JLabel.CENTER);
+                        cell.setSize(100, 100);
+                        cells.add(cell);
+                        cordinate = new Cords(i, j);
+                        matrixCells.add(cordinate);
+                    }
+            int x=0;
+            for (int i=0 ;i<width;i++)
+                for (int j=0;j<height;j++)
+                    {
+                        Image image;
+                        ImageIcon scaled;
+                        Color color = colors.get(rand.nextInt(6));
+                        cells.get(x).setBackground(color);
+                        Color clr = cells.get(x).getBackground();
+                        System.out.println("width"+cells.get(x));
+                        if(clr == Color.red){ 
+                            image = fire.getImage();
+                            Image Simage = image.getScaledInstance(cells.get(x).getWidth(), cells.get(x).getHeight(), Image.SCALE_SMOOTH);
+                            scaled = new ImageIcon(Simage);
+                            cells.get(x).setIcon(scaled);
+                        }else if(clr == Color.blue)
+                        { 
+                            image = water.getImage();
+                            Image Simage = image.getScaledInstance(cells.get(x).getWidth(), cells.get(x).getHeight(), Image.SCALE_SMOOTH);
+                          
+
+                            scaled = new ImageIcon(Simage);
+                            cells.get(x).setIcon(scaled);
+                        }else if(clr == Color.BLACK)
+                        { 
+                            image = wall.getImage();
+                            Image Simage = image.getScaledInstance(cells.get(x).getWidth(), cells.get(x).getHeight(), Image.SCALE_SMOOTH);
+                           
+                            scaled = new ImageIcon(Simage);
+                            cells.get(x).setIcon(scaled);
+                        }else if(clr == Color.yellow)
+                        { 
+                            image = sand.getImage();
+                            Image Simage = image.getScaledInstance(cells.get(x).getWidth(), cells.get(x).getHeight(), Image.SCALE_SMOOTH);
+                            
+                            scaled = new ImageIcon(Simage);
+                            cells.get(x).setIcon(scaled);
+                        }
+                        // cells.get(x) = obstacles.get(rand.nextInt(obstacles.size()));
+                        gameBoard.add(cells.get(x));
+                        x++;
+                    }
+            space.add(gameBoard,BorderLayout.CENTER);
+            solve();
+            space.setVisible(true);
+        }else if(e.getSource() == StartSolving)
+        {
+
+            int R = rand.nextInt(matrixCells.size());
+            cordinate = matrixCells.get(R);
+            // get a Starting Point
+            start = cordinate;
+            R = rand.nextInt(matrixCells.size());
+            cordinate = matrixCells.get(R);
+            //get a Goal Point
+            goal = cordinate;
+            while(start == goal){
+                R = rand.nextInt(matrixCells.size());
+                cordinate = matrixCells.get(R);
+                //get a Goal Point
+                goal = cordinate;
+            }
+            ref =0;
+            for (int i=0 ;i<width;i++)
+                for (int j=0;j<height;j++)
+                    {
+                        if((i == start.getX()) && (j == start.getY()))
+                        {
+                             cells.get(ref).setBackground(Color.orange);
+                        
+                         }else 
+                             if((i == goal.getX()) && (j == goal.getY()))
+                                 cells.get(ref).setBackground(Color.MAGENTA);
+                        
+                    
+                    ref ++;
+                }
+        /*end of Start Solving  */
         }
 
-        gameBoard = new JPanel(new GridLayout(width,height));
-        for (int i=0 ;i<width;i++)
-            for (int j=0; j<height;j++)
-            {
-                cell = new JPanel();
-                cells.add(cell);
-                cordinate = new Cords(i, j);
-                matrixCells.add(cordinate);
-            }
-    int x=0;
-    for (int i=0 ;i<width;i++)
-        for (int j=0;j<height;j++)
-            {
-                Color color = colors.get(rand.nextInt(6));
-                cells.get(x).setBackground(color);
-                gameBoard.add(cells.get(x));
-                x++;
-            }
-    space.add(gameBoard,BorderLayout.CENTER);
-    solve();
-    space.setVisible(true);
     }
     /* Solving method */
-    void solve(){
+    void solve()
+    {
         int width = (int)x.getValue();
         int height = (int)y.getValue();
 
         /* printing the matrix */
         for (Cords cord : matrixCells)
             System.out.print("("+(cord.getX()+1)+","+(cord.getY()+1)+")");
+        StartSolving.setFont(font);
+        StartSolving.addActionListener(this);
+        cordPanel.add(StartSolving);
+        
     }
     public static void main(String argv[]){
         try {
